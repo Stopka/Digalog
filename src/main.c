567fa3c1008758992edcf07ee6fa6aac38527149
Štěpán Škorpil <stepan.s@gmail.com>
2014-01-22 06:45:53 -0800

    Added notification icons

--- src/main.c
+++ src/main.c
@@ -5,7 +5,9 @@
 Window *window;
 BitmapLayer* analog_background;
 BitmapLayer* analog_center[2];
+BitmapLayer* notifications[2];
 RotBitmapLayer* analog_hands[4];
+int battery_state=0;
 Layer* analog;
 Layer* digital_time;
 #define COUNT_TEXTS 4
@@ -35,6 +37,11 @@ static void window_load(Window *window) {
 	analog_background=bitmap_layer_create(GRect(0, 0, 130, 130));
 	bitmap_layer_set_bitmap(analog_background,bitmaps[0]);	
 	layer_add_child(analog,(Layer*)analog_background);
+	
+	notifications[0]=bitmap_layer_create(GRect(57, 24, 16, 16));
+	layer_add_child(analog,(Layer*)notifications[0]);
+	notifications[1]=bitmap_layer_create(GRect(57, 90, 16, 16));
+	layer_add_child(analog,(Layer*)notifications[1]);
 	
 	analog_center[0]=bitmap_layer_create(GRect(59, 59, 12, 12));
 	analog_center[1]=bitmap_layer_create(GRect(59, 59, 12, 12));
@@ -96,16 +103,89 @@ static void update_time(struct tm *tick_time, TimeUnits units_changed){
 	layer_mark_dirty((Layer *)texts[3]);
 }
 
+static void update_bluetooth(bool connected) {
+    GBitmap* btm=(GBitmap*)bitmap_layer_get_bitmap(notifications[1]);
+	if(btm!=NULL&&connected){//prave pripojeno
+		bitmap_layer_set_bitmap(notifications[1],NULL);
+		gbitmap_destroy(btm);
+	}
+	if(btm==NULL&&!connected){//prave odpojeno?
+		bitmap_layer_set_bitmap(notifications[1],gbitmap_create_with_resource(RESOURCE_ID_BLUETOOTH_NONE));
+		vibes_long_pulse();
+	}
+	layer_mark_dirty((Layer *)notifications[1]);
+}
+
+int getBatteryState(BatteryChargeState charge_state){
+	if(charge_state.is_charging){
+		if(charge_state.charge_percent==100){
+			return 1;
+		}
+		if(charge_state.charge_percent>=50){
+			return 2;
+		}
+		return 3;
+	}
+	if(charge_state.charge_percent<5){
+		return 4;
+	}
+	if(charge_state.charge_percent<10){
+		return 5;
+	}
+	return 0;
+}
+
+static void update_battery(BatteryChargeState charge_state) {
+    int state=getBatteryState(charge_state);
+	if(state==battery_state){
+		return;
+	}
+	GBitmap* btm=(GBitmap*)bitmap_layer_get_bitmap(notifications[0]);
+	bitmap_layer_set_bitmap(notifications[0],NULL);
+	gbitmap_destroy(btm);
+	switch(state){
+		case 0:
+			btm=NULL;
+			break;
+		case 1:
+			btm=gbitmap_create_with_resource(RESOURCE_ID_BATTERY_FULL);
+			break;
+		case 2:
+			btm=gbitmap_create_with_resource(RESOURCE_ID_BATTERY_CHARGING_HALF);
+			break;
+		case 3:
+			btm=gbitmap_create_with_resource(RESOURCE_ID_BATTERY_CHARGING_LOW);
+			break;
+		case 4:
+			btm=gbitmap_create_with_resource(RESOURCE_ID_BATTERY_EMPTY);
+			break;
+		case 5:
+			btm=gbitmap_create_with_resource(RESOURCE_ID_BATTERY_LOW);
+			break;
+	}
+	bitmap_layer_set_bitmap(notifications[0],btm);
+	battery_state=state;
+	layer_mark_dirty((Layer *)notifications[0]);
+}
+
 static void window_appear(Window *window) {
+	//Time
 	time_t now_time_t=time(NULL);	
 	struct tm * now_tm=localtime(&now_time_t);
 	update_time(now_tm,SECOND_UNIT);
-	//free(now_tm);
 	tick_timer_service_subscribe(MINUTE_UNIT, update_time);
+	//Bluetooth
+	update_bluetooth(bluetooth_connection_service_peek());
+	bluetooth_connection_service_subscribe(update_bluetooth);
+	//Battery
+	update_battery(battery_state_service_peek());
+	battery_state_service_subscribe(update_battery);
 }
 
 static void window_disappear(Window *window) {
 	tick_timer_service_unsubscribe();
+	bluetooth_connection_service_unsubscribe();
+	battery_state_service_unsubscribe();
 }
 
 static void window_unload(Window *window) {
@@ -122,6 +202,15 @@ static void window_unload(Window *window) {
 	bitmap_layer_destroy(analog_background);
 	bitmap_layer_destroy(analog_center[0]);
 	bitmap_layer_destroy(analog_center[1]);
+	
+	for(int i=0;i<2;i++){
+		GBitmap* btm=(GBitmap*)bitmap_layer_get_bitmap(notifications[i]);
+		bitmap_layer_destroy(notifications[i]);
+		if(btm!=NULL){
+			gbitmap_destroy(btm);	
+		}
+	}
+
 	for(int i=0;i<4;i++){
 		rot_bitmap_layer_destroy(analog_hands[i]);
 	}
